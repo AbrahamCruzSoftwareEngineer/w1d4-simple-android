ef6dc4ba935f4bb1b03cdd3b2f6f01989bb139da
AlbertoMobileApps <beto_ahm@hotmail.com>
2016-10-20 13:22:47 -0500

    my first commit

---
+++ app/src/main/java/com/example/albertocruz/helloworld/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.albertocruz.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void domagic(View view) {
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+        Log.d("TAG_", "doSomething: ");
+
+        Intent intent = new Intent(this, details_activity.class);
+        startActivity(intent);
+
+        TextView textView = (TextView) findViewById(R.id.hello_txt1);
+        textView.setText("Visa ready!");
+    }
+
+    public void domagic2(View view) {
+        Toast.makeText(this, "Hello Atlanta", Toast.LENGTH_SHORT).show();
+        Log.d("TAG_", "doSomething1: ");
+
+        Intent intent = new Intent(this, details_activity.class);
+        startActivity(intent);
+
+        TextView textView = (TextView) findViewById(R.id.hello_txt2);
+        textView.setText("Flying on Sunday");
+
+    }
+
+    public void domagic3(View view) {
+        Toast.makeText(this, "Hello MobileApps", Toast.LENGTH_SHORT).show();
+        Log.d("TAG_", "doSomething3: ");
+
+        Intent intent = new Intent(this, details_activity.class);
+        startActivity(intent);
+
+        TextView textViewtxt1 = (TextView) findViewById(R.id.hello_txt1);
+        textViewtxt1.setText("Hello World!");
+
+        TextView textViewtxt2 = (TextView) findViewById(R.id.hello_txt2);
+        textViewtxt2.setText("Hello Atlanta!");
+
+    }
+}
---
+++ app/src/main/java/com/example/albertocruz/helloworld/details_activity.java
@@ -0,0 +1,13 @@
+package com.example.albertocruz.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class details_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_activity);
+    }
+}
